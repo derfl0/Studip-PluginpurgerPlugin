eb990dc700d84b9effdf586dd0adbcf555cbddac
Flo Bieringer <flo@xited.de>
2013-11-18 22:52:08 +0100

    first working edition

--- controllers/show.php
+++ controllers/show.php
@@ -1,22 +1,38 @@
 <?php
+
 class ShowController extends StudipController {
 
     public function before_filter(&$action, &$args) {
 
-		$this->set_layout($GLOBALS['template_factory']->open('layouts/base_without_infobox'));
+        $this->set_layout($GLOBALS['template_factory']->open('layouts/base_without_infobox'));
 //		PageLayout::setTitle('');
-
     }
 
     public function index_action() {
-
-		$this->answer = 'Yes';
-
+        // gimme some mighty plugin manager
+        $manager = PluginManager::getInstance();
+        
+        // find plugins that are in directory but not loaded
+        foreach (glob("./plugins_packages/*", GLOB_ONLYDIR) as $filename) {
+            $origin = basename($filename);
+            foreach (glob("./plugins_packages/$origin/*", GLOB_ONLYDIR) as $filename) {
+                $pluginname = basename($filename);
+                $ini = parse_ini_file("./plugins_packages/$origin/$pluginname/plugin.manifest");
+                // later i wanna move files
+                //if ($origin != $ini['origin'] || $pluginname != $ini['pluginclassname'] || !$manager->getPlugin($ini['pluginclassname'])) {
+                if (!$manager->getPlugin($ini['pluginclassname'])) {
+                    if (Request::submitted('clean')) {
+                        $manager->registerPlugin($ini['pluginname'], $ini['pluginclassname'], "$origin/$pluginname");
+                    } else {
+                    $this->purge['new'][] = $ini['pluginclassname'];
+                    }
+                }
+            }
+        }
     }
 
     // customized #url_for for plugins
-    function url_for($to)
-    {
+    function url_for($to) {
         $args = func_get_args();
 
         # find params
@@ -30,5 +46,6 @@ class ShowController extends StudipController {
         $args[0] = $to;
 
         return PluginEngine::getURL($this->dispatcher->plugin, $params, join("/", $args));
-    } 
+    }
+
 }
--- views/show/index.php
+++ views/show/index.php
@@ -1,5 +1,13 @@
+<form>
 <? if ($purge): ?>
-    <?= Studip\Button::create(_('Bereinigen')) ?>
+    <? if ($purge['new']): ?>
+        <h1><?= _('Neue Plugins:') ?></h1>
+        <? foreach ($purge['new'] as $new): ?>
+            <p><?= $new ?></p>
+        <? endforeach; ?>
+    <? endif; ?>
+    <?= Studip\Button::create(_('Bereinigen'), 'clean') ?>
 <? else: ?>
     <?= _('Nichts zu reinigen. Alles scheint zu funktionieren.') ?>
 <? endif; ?>
+</form>
